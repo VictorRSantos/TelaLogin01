f048be9a083441648df2331618af513f52b72570
Victor Rodrigues dos Santos <vrsisa@hotmail.com>
2021-01-10 16:22:29 -0300

    Primeiro commit

---
+++ TelaDeLogin01/Login(MVC)/Login(MVC)/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Login_MVC_.Models;
+using System.Data.SqlClient;
+
+namespace Login_MVC_.Controllers
+{
+    public class AccountController : Controller
+    {
+        //Conexão Banco de Dados
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        void connectionString()
+        {
+            con.ConnectionString = "Data Source=DESKTOP\\SQLEXPRESS; database=WPF; integrated security = SSPI;User Id=sa; password = 123456 ";
+          
+        }
+
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = $@"SELECT * FROM tbl_Login WHERE username = '{acc.Name}' AND password = '{acc.Password}'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+
+           
+
+          
+        }
+
+    }
+}
